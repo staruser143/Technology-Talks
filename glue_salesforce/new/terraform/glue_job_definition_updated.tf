5646ed16f8044f555bda59abc27e7494b2a05d57
staruser143 <seriesfan.in@gmail.com>
2025-06-17 15:48:54 +0530

    Create glue_job_definition_updated.tf

---
+++ glue_salesforce/new/terraform/glue_job_definition_updated.tf
@@ -0,0 +1,25 @@
+
+resource "aws_glue_job" "salesforce_jwt_job" {
+  name     = "your-glue-job-name"
+  role_arn = aws_iam_role.glue_salesforce_jwt_role.arn
+
+  command {
+    name            = "pythonshell"
+    python_version  = "3"
+    script_location = "s3://your-script-location/glue_salesforce_jwt_query.py"
+  }
+
+  default_arguments = {
+    "--TempDir"         = "s3://your-temp-dir/"
+    "--job-language"    = "python"
+    "--extra-py-files"  = "s3://your-bucket-name/glue_dependencies.zip"
+  }
+
+  max_capacity = 1.0
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  glue_version = "2.0"
+}
